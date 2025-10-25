8e16f6a463ad7bf269a9cd1bade60f8d28273d80
alle0017 <allegretti0017@gmail.com>
2025-10-25 21:58:27 +0200

    added typing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import Reactive from "./signals/reactive.js";
 import Effect from "./signals/effect.js";
 
-export const signal = value => {const r = new Reactive(); r.value = value; return r; }
+/**
+ * @template T
+ * @param {T} value 
+ * @returns {Reactive<T>}
+ */
+export const signal = value => {
+      const r = new Reactive(); 
+      r.value = value; 
+      return r; 
+}
+/**
+ * @template T
+ * @param {() => T} callback
+ * @param {...Reactive<unknown>} deps
+ * @returns {Reactive<T>}
+ */
 export const effect = (callback, ...deps) => new Effect(callback, ...deps)
+/**
+ * @template T
+ * @param {() => T} callback
+ * @param {...Reactive<unknown>} deps
+ * @returns {Reactive<T>}
+ */
 export const computed = (callback, ...deps) => new Effect(callback, ...deps)
